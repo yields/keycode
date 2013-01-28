6240603259c038d11fd793a83e910e8a4621e612
Amir Abu Shareb <amir.abu.shareb@me.com>
2013-01-28 18:32:30 +0200

    initial

---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+# keycode
+
+  keycode map.
+
+## Installation
+
+    $ component install yields/keycode
+
+## Example
+
+```js
+var code = require('keycode');
+code('enter');
+// > 13
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,42 @@
+
+/**
+ * map
+ */
+
+var map = {
+    backspace: 8
+  , tab: 9
+  , clear: 12
+  , enter: 13
+  , shift: 16
+  , ctrl: 17
+  , alt: 18
+  , capslock: 20
+  , escape: 27
+  , space: 32
+  , left: 37
+  , up: 38
+  , right: 39
+  , down: 40
+  , del: 46
+  , ',': 188
+  , '.': 190
+  , '/': 191
+  , '`': 192
+  , '-': 189
+  , '=': 187
+  , ';': 186
+  , '[': 219
+  , ']': 221
+};
+
+/**
+ * find a keycode.
+ *
+ * @param {String} name
+ * @return {Number}
+ */
+
+module.exports = function(name){
+  return map[name] || name.toUpperCase().charCodeAt(0);
+};
